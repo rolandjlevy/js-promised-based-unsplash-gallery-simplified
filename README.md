cf98a0a2a17e612260c3ba93fe75cbcdcfc357c3
Roland Levy <rolandjlevy@gmail.com>
2021-03-18 20:08:28 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Unplash Gallery simplified
+
+### Links 🔗
+- [Play the demo](https://js-promised-based-unsplash-image-simplified.rolandjlevy.repl.co/)
+- [View the source code](https://replit.com/@RolandJLevy/js-promised-based-unsplash-image-simplified)
+- [Explore the Github repo](https://github.com/rolandjlevy/js-promised-based-unsplash-image-simplified)
---
+++ script.js
@@ -0,0 +1,34 @@
+const $ = (selector) => document.querySelector(selector);
+
+function getSinglePhoto(url) {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+    .then(data => resolve(data.url))
+    .catch(error => reject({error}))
+  });
+}
+
+const baseUrl = "https://source.unsplash.com/random?";
+let counter = 1;
+let max = 60;
+
+while (counter < max) {
+  const randomUrl = `${baseUrl}/${counter}`;
+  const msg = document.createElement('span');
+  msg.textContent = 'Loading...';
+  const img = document.createElement('img');
+  const a = document.createElement('a');
+  a.setAttribute('target', '_blank');
+  a.appendChild(img);
+  const li = document.createElement('li');
+  li.appendChild(msg);
+  li.appendChild(a);
+  $('.container').appendChild(li);
+  getSinglePhoto(randomUrl).then(url => {
+    a.href = url;
+    img.src = url;
+    img.classList.add('fade-in');
+    msg.classList.add('fade-out');
+  });
+  counter++;
+}
